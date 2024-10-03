7ec2f4c875530d0c23626e6b0651b49abd9e5b6f
Manisha K <165355530+manishak8089@users.noreply.github.com>
2024-10-04 05:09:24 +0530

    added file

---
+++ Cpp basics/loops.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //for loop
+    /*for(int i=0;i<=5;i++){
+        cout<<"hello "<< i<<endl;
+    }
+    */
+   
+   //while loop
+    /*int i=1;
+    while(i<=5){
+        cout<<"hello"<<i<<endl;
+        i++;
+    }
+    */
+   
+
+   //do loop
+   int i=2;
+   do {
+    cout<<"hi"<<i<<endl;
+    i++;
+   }while(i<=1);
+}
