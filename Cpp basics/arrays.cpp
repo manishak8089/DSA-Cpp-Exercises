2faf7a98e57beec09bd32a85085cc7b5ec613b05
Manisha K <165355530+manishak8089@users.noreply.github.com>
2024-10-04 04:48:57 +0530

    added files

---
+++ Cpp basics/arrays.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    /* int arr[3];
+    cin >> arr[0]>> arr[1] >> arr[2];
+    arr[1] =10;
+    //arr[1] +=10;
+    cout << arr[1];
+    return 0;
+    */
+
+   //2D array
+   int arr[3][5];
+   arr[1][3]=15;
+   cout<< arr[1][3];
+   return 0;
+}
