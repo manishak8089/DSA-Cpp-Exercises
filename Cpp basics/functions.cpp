e510551fff23dcbb8a3620ea8342fb686794515f
Manisha K <165355530+manishak8089@users.noreply.github.com>
2024-10-16 22:48:43 +0530

    adding files

---
+++ Cpp basics/functions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(string name){
+    cout << "hey "<< name << endl;
+}
+int main(){
+    string name;
+    cin>> name;
+   printName(name);
+
+   string name2;
+    cin>> name2;
+   printName(name2);
+   return 0;
+}
