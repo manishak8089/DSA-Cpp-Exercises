b1a82f2a13bfa80effc5891fe508f437ced68edc
Manisha K <165355530+manishak8089@users.noreply.github.com>
2024-11-07 22:19:15 +0530

    added patterns file

---
+++ Cpp basics/patterns8.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void patterns(int n){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+       int n;
+       cin >> n;
+       patterns(n);
+}
